4122ced0fc23ee512814514f8fcac311becb95de
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-09-06 20:02:15 +0200

    zadatak2

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,17 @@
+<template>
+   <v-ons-toolbar>
+       <div class="center">{{ title }}</div>
+   </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+   props: {
+       title: {
+           type: String,
+           default: 'Github App'
+       }
+   }
+
+}
+</script>
