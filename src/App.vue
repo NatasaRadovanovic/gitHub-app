017f27b78bafa784be5fc48f7ec0e6e07dbc38b7
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-09-06 22:04:46 +0200

    list-tag

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,20 @@
   <div class="center">
     <app-search :query.sync="query"/>
 
-   <v-ons-list-header>Repositories of {{ query }}</v-ons-list-header>
-    
-    <v-ons-list  v-for="repo in repos" :key="repo.id">
-      <div class="left">
-          <img :src="repo.owner.avatar_url" alt="Image" style="width:50px; height:50px">
-        </div>
-        
-        <div class="right">
-          <v-ons-list-item >{{ repo.name }} {{ repo.description }}</v-ons-list-item>
-        </div>
-      </v-ons-list>
-  </div>
-  
+   <v-ons-list>
+      <v-ons-list-header>Repositories of {{ query }}</v-ons-list-header>
+        <v-ons-list-item v-for="repo in repos" :key="repo.id" >
 
- </v-ons-page>
+    <div class="left">
+          <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
+        </div>
+        <div class="center">
+          <span class="list-item__title">{{ repo.name }}</span><span class="list-item__subtitle">{{ repo.description }}</span>
+        </div>
+       </v-ons-list-item>
+    </v-ons-list>
+  </div>
+  </v-ons-page>
 </template>
 
 <script>
